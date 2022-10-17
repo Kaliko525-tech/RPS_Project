23526ab2058c6789b279156900c5770c6535cda8
Kaliko525-tech <kalvinhamilton525@outlook.com>
2022-10-17 16:04:57 +0200

    created Screbord function, made it console.log

--- javascript.js
+++ javascript.js
@@ -28,43 +28,43 @@ function playRound (playerSelection , computerSelection) {
     
 
     if (playerSelection == 'rock' && computerSelection == 'scissors' ) {
-        alert('You win! Rock beats scissors.')
+        console.log('You win! Rock beats scissors.')
 
         return playerScore++;
     }
 
     else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        alert('You lose! Paper beats rock.' )
+        console.log('You lose! Paper beats rock.' )
 
         return computerScore++;
     }
 
     else if(playerSelection == 'scissors' && computerSelection == 'paper'){
-        alert('You win! Scissors beats paper.')
+        console.log('You win! Scissors beats paper.')
 
         return playerScore++;
     }
     
     else if(playerSelection == 'scissors' && computerSelection == 'rock') {
-        alert('You lose! Rock beats scissors.')
+        console.log('You lose! Rock beats scissors.')
 
         return computerScore++;
     }
 
     else if(playerSelection == 'paper' && computerSelection == 'rock') {
-        alert('You win! Paper beats rock.')
+        console.log('You win! Paper beats rock.')
 
         return playerScore++
     }
 
     else if(playerSelection == 'paper' && computerSelection == 'scissors'){
-        alert('You lose! Scissors beats paper.')
+        console.log('You lose! Scissors beats paper.')
 
         return computerScore++;
     }
 
     else {
-        alert('Whoah! It\'s a tie! Nobody Wins!')
+        console.log('Whoah! It\'s a tie! Nobody Wins!')
     }
 
 }
@@ -72,8 +72,23 @@ function playRound (playerSelection , computerSelection) {
 for (let i= 0; i<5; i++){
     getComputerChoice()
     playerInput = prompt('Input rock, paper, or scissors.');
-    playerSelection = playerInput.toLowerCase()
+    playerSelection = playerInput.toLowerCase() ;
     playRound(playerSelection , computerSelection)
 }
 
-alert(`Player Score = ${playerScore} Computer Score = ${computerScore}.`);
+showWinner();
+
+function showWinner () {
+    if (playerScore > computerScore) {
+        console.log(`You beat the computer! Your Score is - ${playerScore} vs ${computerScore} - Computer Score!` )
+    }
+
+    else if (computerScore > playerScore){
+        console.log(`The computer beat you! You Lose! Computer score - ${computerScore} vs ${playerScore} - Your Score!`)
+    }
+
+    else {
+        console.log("Its's a tie!")
+    }
+}
+
